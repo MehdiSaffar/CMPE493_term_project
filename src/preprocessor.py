c514a298069c043b99f590fedd10bdabe90a4534
can <canbolukbas98@gmail.com>
2020-12-31 12:53:45 +0300

    tf_idf calculation corrected

--- src/preprocessor.py
+++ src/preprocessor.py
@@ -21,7 +21,7 @@ def get_idf(N, doc_count):
     return math.log10(N / doc_count)
 
 def get_tf_idf_weight(tf, idf):
-    return 1 + math.log10(tf) * idf
+    return (1 + math.log10(tf)) * idf
 
 class Preprocessor:
     def __init__(self) -> None:
